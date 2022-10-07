9ebb9531179ab75659614b07e61882ea9f4678db
Dionne <dionne.r.9@gmail.com>
2022-10-07 16:31:46 -0400

    setup of homepage and add bootstrap

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+ <script setup>
+  import Header from './components/Header.vue'
+  import HomeIntro from './components/HomeIntro.vue'
+</script>
+
+<template>
+
+<Header />
+
+  <main>
+    <HomeIntro />
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+     <header class="site-header sticky-top py-1">
+  <nav class="container d-flex flex-column flex-md-row justify-content-between">
+    <a class="py-2" href="#" aria-label="Product">
+      <img src="./icons/logo.png" alt="" width="30%">
+    </a>
+    <a class="py-2 d-none d-md-inline-block" href="#">About</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Projects</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Contact</a>
+    <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
+  </nav>
+</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
